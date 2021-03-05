28243950e0e23e0a24707cc26e970a58859e5e59
Silenrate <daniwalteros596@gmail.com>
2021-03-04 21:02:21 -0500

    Basic DB Connection Configuration

--- src/main/java/edu/eci/ieti/envirify/model/Greeting.java
+++ src/main/java/edu/eci/ieti/envirify/model/Greeting.java
@@ -1,20 +1,36 @@
 package edu.eci.ieti.envirify.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "greetings")
 public class Greeting {
 
-    private final long id;
-    private final String content;
+    @Id
+    private String id;
 
-    public Greeting(long id, String content) {
-        this.id = id;
+    private String content;
+
+    public Greeting(String content) {
         this.content = content;
     }
 
-    public long getId() {
+    public Greeting() {
+    }
+
+    public String getId() {
         return id;
     }
 
     public String getContent() {
         return content;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id='" + id + '\'' +
+                ", content='" + content + '\'' +
+                '}';
     }
 }
