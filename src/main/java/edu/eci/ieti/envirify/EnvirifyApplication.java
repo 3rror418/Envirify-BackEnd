3d270d58d51ad257f54867b94966dc757b28d31b
Silenrate <daniwalteros596@gmail.com>
2021-03-06 13:15:16 -0500

    :sparkles: #20 Add User-1.0: Creating Classes

--- src/main/java/edu/eci/ieti/envirify/EnvirifyApplication.java
+++ src/main/java/edu/eci/ieti/envirify/EnvirifyApplication.java
@@ -3,11 +3,21 @@ package edu.eci.ieti.envirify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Class Of Envirify App.
+ *
+ * @author Error 418
+ */
 @SpringBootApplication
 public class EnvirifyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnvirifyApplication.class, args);
-	}
+    /**
+     * Main Function That Runs The Envirify App.
+     *
+     * @param args The extra arguments for the app, do not have functionality.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EnvirifyApplication.class, args);
+    }
 
 }
---
+++ src/main/java/edu/eci/ieti/envirify/persistence/impl/UserPersistenceImpl.java
@@ -0,0 +1,35 @@
+package edu.eci.ieti.envirify.persistence.impl;
+
+import edu.eci.ieti.envirify.exceptions.EnvirifyPersistenceException;
+import edu.eci.ieti.envirify.model.User;
+import edu.eci.ieti.envirify.persistence.UserPersistence;
+import edu.eci.ieti.envirify.persistence.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Class That Implements The User Persistence Methods For Envirify App.
+ *
+ * @author Error 418
+ */
+@Service
+public class UserPersistenceImpl implements UserPersistence {
+
+    @Autowired
+    private UserRepository repository;
+
+    /**
+     * Adds a New User On The DB.
+     *
+     * @param user The User That it is going to be added.
+     * @throws EnvirifyPersistenceException When that user already exists.
+     */
+    @Override
+    public void addUser(User user) throws EnvirifyPersistenceException {
+        User oldUser = repository.findByEmail(user.getEmail());
+        if (oldUser != null) {
+            throw new EnvirifyPersistenceException("There is already a user with the " + user.getEmail() + " email address");
+        }
+        repository.save(user);
+    }
+}
