f56e7a92b58edd248be1e622992700adabdb9ca8
Silenrate <daniwalteros596@gmail.com>
2021-03-13 16:50:00 -0500

    :art: #63 Search Place By Id-1.3: Removing Bad Smells

--- src/main/java/edu/eci/ieti/envirify/controllers/dtos/CreatePlaceDTO.java
+++ src/main/java/edu/eci/ieti/envirify/controllers/dtos/CreatePlaceDTO.java
@@ -5,7 +5,6 @@ import edu.eci.ieti.envirify.model.Place;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Data Transfer Object For Create Place Request.
